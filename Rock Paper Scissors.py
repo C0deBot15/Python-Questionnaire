c989212c2deec01b08ccf3e45525bbb3f3602a71
C0deBot15 <grovesjordan2001@gmail.com>
2025-02-12 13:23:09 +0000

    Add files via upload

---
+++ Rock Paper Scissors.py
@@ -0,0 +1,52 @@
+#Start at the top with import random
+import random
+
+user_wins = 0
+computer_wins = 0
+
+options =["rock", "paper", "scissors"]
+
+while True:
+    user_input = input("Type Rock/Paper/Scissors or Q to quit: ").lower()
+    if user_input == "q":
+        break
+    
+    if user_input not in options:
+        continue
+
+    random_number = random.randint(0, 2)
+    # rock = 0, paper = 1, scissors = 2
+    computer_pick = options[random_number]
+    print("Computer picked", computer_pick + ".")
+
+    if user_input == "rock" and computer_pick == "scissors":
+        print("Winner!")
+        user_wins += 1
+ 
+    elif user_input == "paper" and computer_pick == "rock":
+        print("Winner!")
+        user_wins += 1
+
+    elif user_input == "scissors" and computer_pick == "paper":
+        print("Winner!")
+        user_wins += 1
+    
+    elif user_input == "rock" and computer_pick == "rock":
+        print("It's a draw!")
+        user_wins += 0
+
+    elif user_input == "paper" and computer_pick == "paper":
+        print("It's a draw!")
+        user_wins += 0
+        
+    elif user_input == "scissors" and computer_pick == "scissors":
+        print("It's a draw!")
+        user_wins += 0
+
+    else:
+        print("You lost!")
+        computer_wins += 1
+
+print("You've won", user_wins, "times!")
+print("The machine has won", computer_wins, "times")
+print("Sayonara")
